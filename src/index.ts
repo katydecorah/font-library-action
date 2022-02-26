69cb0b8a27d8ab13d4ef2a57f1600bf433a2d3b1
Katy DeCorah <katydecorah@gmail.com>
2022-02-26 15:11:18 -0500

    Typescript, ncc (#8)
    * TypeScript
    
    * TypeScript
    
    * Workflows
    
    * Lint
    
    * Update documentation
    
    * Update
    
    * Update package.json
    
    * Update documentation
    
    * Create families.json
    
    * Update library.yml
    
    * Updated font library
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+"use strict";
+
+import { getInput, setFailed } from "@actions/core";
+import fetch from "node-fetch";
+import { readFileSync, writeFileSync } from "fs";
+import stringify from "json-stringify-pretty-compact";
+
+async function library() {
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/webfonts/v1/webfonts?key=${getInput(
+        "GoogleToken"
+      )}`
+    );
+    const library = (await response.json()) as { items: [] };
+    // build list of family names in Google Fonts API
+    const remoteFonts = library.items.map(({ family }) => family);
+
+    // get list of families in font library
+    let local = JSON.parse(readFileSync("families.json", "utf-8"));
+    const localFonts = local.map((font) => font.family);
+    // get difference between remote and local libraries
+    const diff = remoteFonts.filter((x) => !localFonts.includes(x));
+
+    if (diff.length > 0) {
+      // add diff to localFonts
+      diff.map((font) => local.push({ family: font, tags: [] }));
+      // sort by "family"
+      local = local.sort((a, b) => (a.family > b.family ? 1 : -1));
+      // write file
+      writeFileSync(
+        "families.json",
+        stringify(local, { maxLength: 200 }),
+        "utf-8"
+      );
+      console.log(`Updated library: ${diff.join(", ")}`);
+    } else {
+      console.log("Nothing to update.");
+    }
+  } catch (error) {
+    setFailed(error.message);
+  }
+}
+
+export default library();
